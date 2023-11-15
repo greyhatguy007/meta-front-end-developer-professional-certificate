ab4205201f280fa8f342b12ff6e3443895f53f42
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-11-15 14:02:03 +0530

    onChange is written to perform themeToggle
    ### **Step 3**
    
    Open the `Switch/index.js` file. Add an `onChange` prop to the input element and pass as the event handler a callback function to change the theme.
    You don’t need to use the event argument in this case.
    
    Please merge this

--- C6-Advanced-React/week1/lab-create-a-light-dark-theme-switcher/src/Switch/index.js
+++ C6-Advanced-React/week1/lab-create-a-light-dark-theme-switcher/src/Switch/index.js
@@ -2,12 +2,13 @@ import "./Styles.css";
 import { useTheme } from "../ThemeContext";
 
 const Switch = () => {
-  const { theme } = useTheme();
+  const { theme, toggleTheme } = useTheme();
   return (
     <label className="switch">
       <input
         type="checkbox"
         checked={theme === "light"}
+        onChange={toggleTheme}
       />
       <span className="slider round" />
     </label>
